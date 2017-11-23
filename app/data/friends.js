e6da117052276c3d2255aaab4cdc32410df44cd6
Jarrod <jwmcenroe@gmail.com>
2017-11-23 11:12:02 -0800

    Added friends objects

---
+++ app/data/friends.js
@@ -0,0 +1,88 @@
+var friends = [
+   {
+      "name":"Tanya",
+      "photo":"https://media.npr.org/assets/img/2016/12/13/_dsc2422_custom-ec822c56f6ac227665c344c11398217f64bbf7a5-s200-c85.jpg",
+      "scores":[
+         "5",
+         "1",
+         "4",
+         "4",
+         "5",
+         "1",
+         "2",
+         "5",
+         "4",
+         "1"
+      ]
+   },
+
+   {
+      "name":"Eleanor",
+      "photo":"https://media.npr.org/about/people/bios/biophotos/EBeardsley_custom-7430afe6ac5463f4cb34465b1d9a44878726a3bd-s200-c85.jpg"
+      "scores":[
+         "5"
+         "4"
+         "2"
+         "4"
+         "3"
+         "4"
+         "1"
+         "4"
+         "4"
+         "3"
+      ]
+   },
+
+   {
+      "name":"Karen",
+      "photo":"https://media.npr.org/about/people/bios/biophotos/kbates_2006_custom-7cdf4fd08e27c223d3acb64b4af7245acf8e453c-s200-c85.jpg"
+      "scores":[
+         "1"
+         "2"
+         "2"
+         "5"
+         "3"
+         "2"
+         "4"
+         "3"
+         "2"
+         "1"
+      ]
+   },
+
+   {
+      "name":"Alisa",
+      "photo":"https://media.npr.org/assets/img/2017/04/13/ailsa-chang_npr_1_vert-25d3645f1cacfaa9c9e3db8a6beb2c2c037a945b-s200-c85.jpg"
+      "scores":[
+         "4"
+         "1"
+         "2"
+         "5"
+         "5"
+         "4"
+         "2"
+         "3"
+         "2"
+         "2"
+      ]
+   },
+
+   {
+      "name":"Leila",
+      "photo":"https://media.npr.org/assets/img/2012/09/10/headshot_edited_vert-2e6ec33c54fa5f26c99ed9a9931b88983fc816dc-s200-c85.jpg"
+      "scores":[
+         "5"
+         "5"
+         "3"
+         "4"
+         "5"
+         "2"
+         "4"
+         "3"
+         "3"
+         "3"
+      ]
+   }
+
+
+
